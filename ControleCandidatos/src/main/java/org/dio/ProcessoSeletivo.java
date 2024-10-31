220336250e244f587add540f1fe0eab9a1f7f179
Rodrigo Christofani <christofanir@gmail.com>
2024-10-31 12:01:13 -0300

    Adicionando lógica de seleção de candidatos.

--- ControleCandidatos/src/main/java/org/dio/ProcessoSeletivo.java
+++ ControleCandidatos/src/main/java/org/dio/ProcessoSeletivo.java
@@ -1,10 +1,32 @@
 package org.dio;
 
+import java.util.concurrent.*;
+
 public class ProcessoSeletivo {
   public static void main(String[] args) {
-    analisarCandidato(1900.0);
-    analisarCandidato(2200.0);
-    analisarCandidato(2000.0);
+    selecaoDeCandidatos();
+  }
+
+  static void selecaoDeCandidatos() {
+    String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+    int candidatosSelecionados = 0;
+    int candidatosAtual = 0;
+    double salarioBase = 2000.0;
+    while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+      String candidato = candidatos[candidatosAtual];
+      double salarioPretendido = valorPretendido();
+
+      System.out.println("Candidato: " + candidato + " Solicitou esse valor de salário " + salarioPretendido);
+      if (salarioBase >= salarioPretendido) {
+        System.out.println("O candidato " + candidato + " foi selecionado para a vaga ");
+        candidatosSelecionados++;
+      }
+      candidatosAtual++;
+    }
+  }
+
+  public  static double valorPretendido() {
+    return ThreadLocalRandom.current().nextDouble(1800, 2000);
   }
 
   public static void analisarCandidato(double salarioPretendido) {
