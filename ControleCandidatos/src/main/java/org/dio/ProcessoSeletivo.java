5fcfb95dbc9145b45a0f9e1318d3d5b6ac923534
Rodrigo Christofani <christofanir@gmail.com>
2024-11-01 10:19:57 -0300

    Adicionando lógica de ligar para os candidatos !

--- ControleCandidatos/src/main/java/org/dio/ProcessoSeletivo.java
+++ ControleCandidatos/src/main/java/org/dio/ProcessoSeletivo.java
@@ -1,10 +1,52 @@
 package org.dio;
 
+import java.util.*;
 import java.util.concurrent.*;
 
 public class ProcessoSeletivo {
   public static void main(String[] args) {
-    selecaoDeCandidatos();
+    String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+    for (String candidato : candidatos) {
+      entrandoEmContato(candidato);
+    }
+  }
+
+  static void entrandoEmContato(String candidato) {
+    int tentativasRealizadas = 1;
+    boolean continuaTentando = true;
+    boolean atendeu = false;
+    do {
+      atendeu = atender();
+      continuaTentando = !atendeu;
+      if (continuaTentando) {
+        tentativasRealizadas++;
+      } else {
+        System.out.println("Contato realizado com sucesso!");
+      }
+    } while (continuaTentando && tentativasRealizadas< 3);
+    if (atendeu) {
+      System.out.println("Conseguimos contato com " + candidato + " Na " + tentativasRealizadas + " tentativas!");
+    } else  {
+      System.out.println("Não foi possível contato com " + candidato + " Numero máximo de tentativas! " + tentativasRealizadas + " tentativas!");
+    }
+  }
+
+  // Se o valor entre 3 for igual a 1 quer dizer que atendeu !
+  static boolean atender() {
+    return new Random().nextInt(3) == 1;
+  }
+
+  static void imprimirSelecionados() {
+    String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+    System.out.println("Informando a lista de candidatos informando o índice do elemento");
+    for (int indice = 0; indice < candidatos.length; indice++) {
+      System.out.println("O candidato de n° " + (indice+1) + " é o " + candidatos[indice]);
+    }
+
+    System.out.println("Forma abreviada de interação for each");
+    for (String candidato : candidatos) {
+      System.out.println("O candidato selecionado foi " + candidato);
+    }
   }
 
   static void selecaoDeCandidatos() {
